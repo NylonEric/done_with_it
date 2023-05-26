ee23f5afe2b3038e1feaefc24e77dfcb86d52629
NylonEric <51034223+NylonEric@users.noreply.github.com>
2023-05-25 23:58:15 -0400

    app working with style animation

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { FadeInView } from './animations/fade_in_view';
 
-export default function App() {
+export default App = () => {
+  console.log('App executed');
+ 
+  let x = 1;
+  x.toString();
+
   return (
     <View style={styles.container}>
-      <Text>Hi Kenny!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <FadeInView style={styles.container}>
+        <Text style={styles.text}>HELLO WORLD!</Text>
+        <Text style={styles.text}>THIS IS THE BEST APP EVER!</Text>
+        <Text style={styles.subtitle}>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+        </FadeInView>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgrey',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: 'blue',
+    justifyContent: 'center',
+  },
+  subtitle: {
+    fontSize: 12,
+    color: 'black'
+  }
 });
---
+++ animations/fade_in_view.js
@@ -0,0 +1,24 @@
+import { Animated } from 'react-native';
+import React, {useRef, useEffect} from 'react';
+
+export const FadeInView = props => {
+  const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 10000,
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim]);
+
+  return (
+    <Animated.View // Special animatable View
+      style={{
+        ...props.style,
+        opacity: fadeAnim, // Bind opacity to animated value
+      }}>
+      {props.children}
+    </Animated.View>
+  );
+};
