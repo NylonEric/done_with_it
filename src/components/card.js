81fa3e4912965031a52c59c94d063ebde39aeb24
NylonEric <51034223+NylonEric@users.noreply.github.com>
2023-08-14 23:54:37 -0400

    basic component

---
+++ src/components/card.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import styles from '../../styles/styles';
+
+const Card = ({title, subtitle, imageSource}) => {
+
+};
+
+export default Card;
