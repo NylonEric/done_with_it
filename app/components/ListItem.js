1329e720be31646154d40752f81bdf15e32d6c4e
NylonEric <51034223+NylonEric@users.noreply.github.com>
2023-09-09 01:57:02 -0400

    touchableHighlight

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,16 +1,22 @@
-import { View, Image } from 'react-native'
+import { View, Image, TouchableHighlight } from 'react-native'
 import React from 'react'
 import { styles } from '../styles/styles';
+import colors from '../config/colors';
 import AppText from './AppText';
 
-export default function ListItem({title, subTitle, image}) {
+export default function ListItem({title, subTitle, image, onPress}) {
   return (
-    <View style={styles.listContainer}>
-      <Image source={image} style={styles.listImage}/>
-      <View>
-        <AppText style={styles.userTitle}>{title}</AppText>
-        <AppText style={styles.userSubTitle}>{subTitle}</AppText>
+    <TouchableHighlight 
+      onPress={onPress}
+      underlayColor={colors.light}
+      >
+      <View style={styles.listContainer}>
+        <Image source={image} style={styles.listImage}/>
+        <View>
+          <AppText style={styles.userTitle}>{title}</AppText>
+          <AppText style={styles.userSubTitle}>{subTitle}</AppText>
+        </View>
       </View>
-    </View>
+    </TouchableHighlight>    
   )
 }
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -30,6 +30,7 @@ export default function MessagesScreen() {
             title={item.title}
             subTitle={item.description}
             image={item.image}
+            onPress={()=> console.log("Message Selected: ", item)}
           />
         }
         ItemSeparatorComponent={ListItemSeparator}
