7a1655e79cfafd7b2f765a310dfc28573520504e
NylonEric <51034223+NylonEric@users.noreply.github.com>
2023-09-22 15:32:35 -0400

    messages drag screen flex to fill space

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import { View, SafeAreaView } from 'react-native'
+import { View, SafeAreaView, StyleSheet } from 'react-native'
 import { styles } from '../styles/styles';
 
 export default function Screen({ children, style }) {
   return (
     <SafeAreaView style={[styles.screen, style]}>
-      <View style={style}>
+      <View style={[localStyles.view, style]}>
         {children}
       </View>
     </SafeAreaView>
   )
 };
+
+const localStyles = StyleSheet.create({
+  view: {
+    flex: 1,
+  },
+});
